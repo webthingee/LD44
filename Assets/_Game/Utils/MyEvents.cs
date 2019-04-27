56fd82a716360c1fe3df5d6f0f1bb2ecb9846e2d
webthingee <sean@webthingee.com>
2019-04-27 08:44:53 -0700

    floor pointing

---
+++ Assets/_Game/Utils/MyEvents.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+namespace MyEvents
+{
+    public abstract class Event<T> where T : Event<T> 
+    {
+        public string description;
+
+        private bool hasFired;
+        
+        public delegate void EventListener(T e);
+        private static event EventListener listeners;
+        
+        public static void RegisterListener(EventListener listener) {
+            listeners += listener;
+        }
+
+        public static void UnregisterListener(EventListener listener) {
+            listeners -= listener;
+        }
+
+        public void FireEvent() {
+            if (hasFired) {
+                throw new Exception("This event has already fired, to prevent infinite loops you can't refire an event");
+            }
+        
+            hasFired = true;
+            
+            if (listeners != null) {
+                listeners(this as T);
+            }
+        }
+    }
+
+    public class DebugEvent : Event<DebugEvent>
+    {
+        public int verbosityLevel;
+    }
+    
+    public class FloorPointEvent : Event<FloorPointEvent>
+    {
+        public GameObject obj;
+        public Vector2 floorPoint;
+    }
+    
+    //Examples
+//    public class UnitHealthEvent : Event<UnitHealthEvent>
+//    {
+//        public GameObject gObj;
+//        public bool isDead;
+//    }
+//    
+//    public class UnitAttackEvent : Event<UnitAttackEvent>
+//    {
+//        public int gObjId;
+//    }
+}
---
+++ Assets/_Game/Utils/PointerMaster.cs
@@ -0,0 +1,61 @@
+﻿using MyEvents;
+using UnityEngine;
+
+public class PointerMaster : MonoBehaviour
+{
+    public LayerMask floorLayer;
+
+    private LineRenderer _lineRenderer;
+
+    private void Awake()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        // center on mouse
+        var pos = Input.mousePosition;
+        pos.z = 10;
+        pos = Camera.main.ScreenToWorldPoint(pos);
+        transform.position = pos;
+
+        DisplayFloorLine(transform.position, GetFloorPoint(transform.position));
+    }
+
+    void DisplayFloorLine (Vector3 _startingPoint, Vector3 _floorPoint)
+    {
+        Vector3[] positions = new Vector3[2];
+        positions[0] = _startingPoint;
+        positions[1] = _floorPoint;
+        
+        _lineRenderer.SetPositions(positions);
+        _lineRenderer.startWidth = 0.05f;
+
+        _lineRenderer.enabled = _startingPoint != _floorPoint;
+    }
+
+    public Vector3 GetFloorPoint(Vector3 _startingPoint)
+    {
+        var rayStart = _startingPoint;
+        var rayDir = Vector2.down;
+        float rayDist = 10.0f;
+
+        Vector2 floorPosition = Vector2.zero;
+        
+        RaycastHit2D hit = Physics2D.Raycast(rayStart, rayDir, rayDist, floorLayer); //1 << LayerMask.NameToLayer("Enemy"));
+        if (hit)
+        {
+            floorPosition = hit.point;
+        }
+        
+        FloorPointEvent OnFloorPointEvent = new FloorPointEvent
+        {
+            description = "Unit " + gameObject.name + " Health Event.", 
+            floorPoint = floorPosition
+        };
+        OnFloorPointEvent.FireEvent();
+
+        return floorPosition;
+    }
+}
