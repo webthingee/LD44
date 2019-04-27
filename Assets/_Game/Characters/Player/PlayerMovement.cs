c52b4866927420193e740523c827e775af823a71
webthingee <sean@webthingee.com>
2019-04-27 09:23:55 -0700

    movement w mouse

--- Assets/_Game/Characters/Player/PlayerMovement.cs
+++ Assets/_Game/Characters/Player/PlayerMovement.cs
@@ -1,32 +1,5 @@
-﻿//using MyEvents;
-//using UnityEngine;
-
-//public class PlayerMovement : MonoBehaviour
-//{
-//    private Vector2 floorPoint;
-    
-//    private void OnEnable()
-//    {
-//        FloorPointEvent.RegisterListener(OnFloorPointEvent);
-//    }
-
-//    private void Update()
-//    {
-//        if (Input.GetMouseButtonUp(0))
-//        {
-//            if (floorPoint != null) transform.position = floorPoint;
-//        }
-//    }
-
-//    private void OnFloorPointEvent(FloorPointEvent e)
-//    {
-//        //Debug.Log("PlayerMovement sees floor point at: " + e.floorPoint);
-//        floorPoint = e.floorPoint;
-//    }
-//}
-
+﻿using System;
 using UnityEngine;
-using System.Collections;
 using MyEvents;
 using Prime31;
 
@@ -40,19 +13,18 @@ public class PlayerMovement : MonoBehaviour
 	public float jumpHeight = 3f;
 	
 	private Vector2 floorPoint;
+	private Vector2 gotoFloorPoint;
 
 	[HideInInspector]
 	private float normalizedHorizontalSpeed = 0;
 
 	private CharacterController2D _controller;
-	private Animator _animator;
 	private RaycastHit2D _lastControllerColliderHit;
 	private Vector3 _velocity;
 
 
 	void Awake()
 	{
-		_animator = GetComponent<Animator>();
 		_controller = GetComponent<CharacterController2D>();
 
 		// listen to some events for illustration purposes
@@ -95,50 +67,43 @@ public class PlayerMovement : MonoBehaviour
 
 	// the Update loop contains a very simple example of moving the character around and controlling the animation
 	void Update()
-	{
-		
+	{		
 		if (Input.GetMouseButtonUp(0))
 		{
-			if (floorPoint != null) transform.position = floorPoint;
+			if (floorPoint != null) gotoFloorPoint = floorPoint;
 		}
-		
-		if( _controller.isGrounded )
+
+		if (_controller.isGrounded)
+		{
 			_velocity.y = 0;
-
-		if( Input.GetKey( KeyCode.RightArrow ) )
-		{
-			normalizedHorizontalSpeed = 1;
-			if( transform.localScale.x < 0f )
-				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
-
-			if( _controller.isGrounded )
-				_animator.Play( Animator.StringToHash( "Run" ) );
 		}
-		else if( Input.GetKey( KeyCode.LeftArrow ) )
+			
+		if (Mathf.Abs(transform.position.x - gotoFloorPoint.x) > 0.3f)
 		{
-			normalizedHorizontalSpeed = -1;
-			if( transform.localScale.x > 0f )
-				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
 
-			if( _controller.isGrounded )
-				_animator.Play( Animator.StringToHash( "Run" ) );
+
+			if (transform.position.x - gotoFloorPoint.x > 0.2f)
+			{
+				normalizedHorizontalSpeed = -1;
+				if (transform.localScale.x > 0f)
+				{
+					transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, 0);
+				}
+			}
+			else if (transform.position.x - gotoFloorPoint.x < 0.2f)
+			{
+				normalizedHorizontalSpeed = 1;
+				if (transform.localScale.x < 0f)
+				{
+					transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, 0);
+				}
+			}
 		}
 		else
 		{
 			normalizedHorizontalSpeed = 0;
-
-			if( _controller.isGrounded )
-				_animator.Play( Animator.StringToHash( "Idle" ) );
+			Debug.Log("no speed" + _velocity.y);
 		}
-
-
-		// we can only jump whilst grounded
-		if( _controller.isGrounded && Input.GetKeyDown( KeyCode.UpArrow ) )
-		{
-			_velocity.y = Mathf.Sqrt( 2f * jumpHeight * -gravity );
-			_animator.Play( Animator.StringToHash( "Jump" ) );
-		}
-
 
 		// apply horizontal speed smoothing it. dont really do this with Lerp. Use SmoothDamp or something that provides more control
 		var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
@@ -147,15 +112,11 @@ public class PlayerMovement : MonoBehaviour
 		// apply gravity before moving
 		_velocity.y += gravity * Time.deltaTime;
 
-		// if holding down bump up our movement amount and turn off one way platform detection for a frame.
-		// this lets us jump down through one way platforms
-		if( _controller.isGrounded && Input.GetKey( KeyCode.DownArrow ) )
-		{
-			_velocity.y *= 3f;
-			_controller.ignoreOneWayPlatformsThisFrame = true;
-		}
-
+		if (normalizedHorizontalSpeed == 0) _velocity.y = 0;
+		
 		_controller.move( _velocity * Time.deltaTime );
+
+		Debug.Log(_velocity.y);
 
 		// grab our current _velocity to use as a base for all calculations
 		_velocity = _controller.velocity;
