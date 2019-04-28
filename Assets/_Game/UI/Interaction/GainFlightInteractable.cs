8b0384dead99ef081a2817c41ccfc9e4ea5de906
webthingee <sean@webthingee.com>
2019-04-27 23:39:20 -0700

    flight

---
+++ Assets/_Game/UI/Interaction/GainFlightInteractable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GainFlightInteractable : Interactable
+{
+    //public GameObject door;
+    //public GameObject destination;
+    
+    protected override void PowerLevelCheck()
+    {
+        Debug.Log("powerCheck");
+        if (FindObjectOfType<PlayerPower>().PlayerPowerLevel >= powerReqiured)
+        {
+            inProgress = false;
+            isUnlocked = true;
+
+            FindObjectOfType<PlayerFlight>().enabled = true;
+            GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().gravityScale = 0;
+
+        }
+    }
+}
