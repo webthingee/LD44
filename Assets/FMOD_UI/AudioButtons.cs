413e971a6d023efe8c3a904be41b60a7f5c276dc
webthingee <sean@webthingee.com>
2019-05-02 06:57:40 -0700

    bug fix 0.1 - exit trigger overlap

--- Assets/FMOD_UI/AudioButtons.cs
+++ Assets/FMOD_UI/AudioButtons.cs
@@ -4,14 +4,28 @@ using UnityEngine.UI;
 public class AudioButtons : MonoBehaviour
 {
     public Button closeButton;
+    public Button quitButton;
     
     private void Start()
     {
         closeButton?.GetComponent<Button>().onClick.AddListener(ToggleActive);
+        quitButton?.GetComponent<Button>().onClick.AddListener(QuitGame);
     }
 
     public void ToggleActive()
     {
         GetComponentInParent<AudioMaster>().ToggleActive();
+    }
+    
+    public void QuitGame() 
+    {
+        #if UNITY_EDITOR // If we're in Unity Editor, stop play mode
+        if (UnityEditor.EditorApplication.isPlaying == true)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        #else
+            Application.Quit();
+        #endif
     }
 }
--- Assets/_Game/Utils/MainMusic.cs
+++ Assets/_Game/Utils/MainMusic.cs
@@ -13,10 +13,36 @@ public class MainMusic : MonoBehaviour
         mainMusicEvent = RuntimeManager.CreateInstance(mainMusicSelection);
         mainMusicEvent.setParameterByName("SongSelection", 0, true);
         mainMusicEvent.start();
+        mainMusicEvent.release();
+    }
+
+    public void ChangeSongSelection(float songNum)
+    {
+        mainMusicEvent.setParameterByName("SongSelection", songNum, true);
+        songSelectionValue = songNum;
     }
 
     void Update()
     {
         mainMusicEvent.setParameterByName("SongSelection", songSelectionValue, true);
+    }
+
+    void ThreeDeeAttach()
+    {
+        FMOD.Studio.EventInstance threeDeeEvent = RuntimeManager.CreateInstance("event:/3D/...");
+        threeDeeEvent.start();
+        RuntimeManager.AttachInstanceToGameObject(threeDeeEvent, transform, transform.GetComponent<Rigidbody>());
+        threeDeeEvent.release();
+    }
+
+    void Once()
+    {
+        RuntimeManager.PlayOneShotAttached("event:/SFX/...", gameObject);
+        RuntimeManager.PlayOneShot("event:/SFX/...", new Vector3(1f, 1f, 1f));
+    }
+
+    private void OnDestroy()
+    {
+        mainMusicEvent.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
     }
 }
